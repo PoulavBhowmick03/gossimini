607b4158a9e0ecca35d7ef7dccbebb5762d6aefc
PoulavBhowmick03 <bpoulav@gmail.com>
2025-08-15 19:53:53 +0530

    Initial impl of Node and wrap it with Swarm

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,14 +1,68 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::Result;
+use libp2p::{
+    Multiaddr, PeerId, SwarmBuilder,
+    futures::StreamExt,
+    identify, identity, ping,
+    swarm::{NetworkBehaviour, Swarm, SwarmEvent},
+};
+use tracing::info;
+
+#[derive(NetworkBehaviour)]
+pub struct NodeBehaviour {
+    identify: identify::Behaviour,
+    ping: ping::Behaviour,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl NodeBehaviour {
+    fn new(local_public: &identity::PublicKey) -> Self {
+        let identify = identify::Behaviour::new(identify::Config::new(
+            "/gossimini/1.0.0".into(),
+            local_public.clone(),
+        ));
+        let ping = ping::Behaviour::default();
+        Self { identify, ping }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Node {
+    pub peer_id: PeerId,
+    pub swarm: Swarm<NodeBehaviour>,
+}
+
+impl Node {
+    pub async fn new() -> Result<Self> {
+        let swarm = SwarmBuilder::with_new_identity()
+            .with_tokio()
+            .with_tcp(
+                Default::default(),
+                (libp2p_tls::Config::new, libp2p_noise::Config::new),
+                libp2p_yamux::Config::default,
+            )?
+            .with_quic()
+            .with_behaviour(|keypair| NodeBehaviour::new(&keypair.public()))?
+            .build();
+        let peer_id = *swarm.local_peer_id();
+        Ok(Self { peer_id, swarm })
+    }
+
+    fn listen_on(&mut self, addr: Multiaddr) -> Result<()> {
+        self.swarm.listen_on(addr)?;
+        Ok(())
+    }
+
+    pub async fn run(mut self) -> Result<()> {
+        loop {
+            match self.swarm.select_next_some().await {
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    println!("Address: {}", address);
+                }
+                SwarmEvent::Behaviour(ev) => {
+                    println!("ev: {:?}", ev);
+                }
+                other => {
+                    info!(?other, "swarm");
+                }
+            }
+        }
     }
 }
